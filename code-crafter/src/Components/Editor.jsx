a07fe6b9be63889b62933e35a61ba5f6be3d42dd
ASWIN SAMUVEL E <aswin42samuel7@gmail.com>
2025-04-07 22:57:16 +0530

    completed

--- code-crafter/src/Components/Editor.jsx
+++ code-crafter/src/Components/Editor.jsx
@@ -5,23 +5,28 @@ import "codemirror/mode/xml/xml";
 import "codemirror/mode/css/css";
 import "codemirror/mode/javascript/javascript";
 import { Controlled as ControlledEditor } from "react-codemirror2";
-import texteditor from "./Store";
+// import texteditor from "./Store";
 import { BiSolidShare } from "react-icons/bi";
 import { SlShareAlt } from "react-icons/sl";
 
 const Editor = ({ language, title, value, onChange }) => {
-//   const open = texteditor((state) => state.open);
-//   const toggle = texteditor((state) => state.toggle);
+  //   const open = texteditor((state) => state.open);
+  //   const toggle = texteditor((state) => state.toggle);
 
-const [open,setOpen] =  useState(false)
+  const [open, setOpen] = useState(true);
   const handleChange = (editor, data, value) => {
     onChange(value);
   };
   return (
-    <div className={`editor-container ${open ? "": "collapse"}` }>
+    <div className={`editor-container ${open ? "" : "collapse"}`}>
       <div className="editor-title">
         {title}
-        <button className="expand-collapse-button" onClick={()=>setOpen((pre)=>!pre)}>{open ?<BiSolidShare />:<SlShareAlt />}</button>
+        <button
+          className="expand-collapse-button"
+          onClick={() => setOpen((pre) => !pre)}
+        >
+          {open ? <BiSolidShare /> : <SlShareAlt />}
+        </button>
       </div>
       <ControlledEditor
         onBeforeChange={handleChange}
